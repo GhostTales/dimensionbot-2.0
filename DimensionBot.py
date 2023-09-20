5ae6d3d0e1d015d15b3f4b420c9d9d31e0a0d854
GhostTales <danjuulchristensen1@gmail.com>
2023-09-20 21:18:00 +0200

    moved everything from private stuff to its own

---
+++ DimensionBot.py
@@ -0,0 +1,375 @@
+import contextlib
+import datetime
+import json
+import os
+import random
+import shutil
+import time
+import urllib.request
+from PIL import Image
+import discord
+from discord.ext import tasks, commands
+import re
+from osu_commands import osu_stats, linking
+
+def run():
+    intents = discord.Intents.all()
+    bot = commands.Bot(command_prefix='!', intents=intents)
+    bot.remove_command('help')
+
+    @bot.command()
+    async def link(ctx, string=''):
+        # Write to JSON with guild id and string as value
+        try:
+            links = linking(string)
+            user = links.user
+            # Check if the user is already linked to an account
+            with open('osu_links.json', 'r') as file:
+                data = json.load(file)
+            data[ctx.author.id] = user.id if ctx.author.id in data else user.id
+            with open('osu_links.json', 'w') as file:
+                json.dump(data, file)
+
+            embed = discord.Embed(description=f'succesfully linked {ctx.author.mention} to {string}',
+                                  colour=discord.Colour.orange())
+            await ctx.send(embed=embed)
+        except Exception as e:
+            print(f"Error linking account: {e}")
+            await ctx.send(embed=discord.Embed(description=f'{ctx.author.mention} Error linking. Please make sure the account exists.',
+                                               colour=discord.Colour.red()))
+
+    @bot.command()
+    async def rs(ctx):
+        member = ctx.author.id
+        member = str(member)
+        stats = ''
+
+        with open('osu_links.json', 'r') as file:
+            data = json.load(file)
+        if member in data:
+            user = data[member]
+            try:
+                stats = osu_stats(user=user, play_type='recent', mode='osu')
+                play = stats.play
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                play = None
+
+            # print(play)
+            # print(stats.MapInfo)
+            if play is not None and play != "":
+
+                pp = f'{stats.stat_pp}PP ({stats.stat_fc_pp}PP if fc)'
+                acc = f'{stats.stat_acc}% ({stats.stat_fc_acc}% if fc)\n'
+
+                if stats.stat_fc_acc == stats.stat_acc:
+                    pp = f'{stats.stat_pp}PP'
+                    acc = f'{stats.stat_acc}% '
+
+                hit = f'[{stats.stat_n300}/{stats.stat_n100}/{stats.stat_n50}/{stats.stat_n_miss}]'
+                map_stats = f'BPM: {stats.map_bpm} AR: {stats.map_ar} OD: {stats.map_od} CS: {stats.map_cs} HP: {stats.map_hp}'
+
+                if float(stats.stat_map_progress) != 100.0:
+                    progress = f'▸ ({stats.stat_map_progress}%)'
+                else: progress = ''
+
+
+                recent = discord.Embed(description=f'{progress} ▸ {acc}▸ {pp}\n'
+                                                   f'▸ {stats.stat_score} ▸ x{stats.stat_achieved_combo}/{stats.map_max_combo} ▸ {hit}\n'
+                                                   f'▸ {map_stats}', colour=discord.Colour.orange())
+
+
+                # base image location to download from osu map
+                base_image_pos = "map_image_card/map_card.png"
+                # download map card
+                urllib.request.urlretrieve(stats.map_image, base_image_pos)
+
+                # open all images for editing
+                base_image = Image.open(base_image_pos)
+                middle_image = Image.open('map_image_card/rectangle.png')
+                top_image = Image.open(f'rank_grades/{stats.stat_rank_grade}.png')
+
+                # edit all the images together
+                base_image.paste(middle_image.resize((100, 100)), (-30, -65), middle_image.resize((100, 100)))
+                base_image.paste(top_image.resize((48, 24)), (10, 0), top_image.resize((48, 24)))
+
+                # save the finished image
+                base_image.save(base_image_pos, quality=95)
+
+                # make the saved image a discord.file attachment
+                map_card = discord.File(base_image_pos, filename='map_card.png')
+
+                recent.set_image(url='attachment://map_card.png')
+
+                recent.set_footer(icon_url=stats.player_avatar, text=f'{stats.player_name}  |  On osu! Bancho ')
+                recent.timestamp = stats.map_date_created
+
+                rank_status = discord.File(f'ranking_status/{stats.map_rank_status}.png', filename=f'{stats.map_rank_status}.png')
+                recent.set_author(name=f'{stats.map_title} [{stats.map_diff}] +{stats.stat_mods} [{stats.stat_stars}★]',
+                                  url=f'https://osu.ppy.sh/beatmapsets/{stats.mapset_id}#osu/{stats.map_id}',
+                                  icon_url=f'attachment://{stats.map_rank_status}.png')
+
+                await ctx.send(files=[rank_status, map_card], embed=recent)
+            else:
+                await ctx.send(embed=discord.Embed(description="No recent play data available",
+                                                   colour=discord.Colour.red()))
+        else:
+            await ctx.send(embed=discord.Embed(description=f'{ctx.author.mention} is not linked to an osu account',
+                                               colour=discord.Colour.red()))
+
+    @bot.command()
+    async def roll(ctx, number=100):
+        embed = discord.Embed(description=f'{ctx.author.mention} rolled {random.randint(1, number)}',
+                              colour=discord.Colour.orange())
+        await ctx.send(embed=embed)
+
+    @roll.error
+    async def roll_error(ctx, error):
+        if isinstance(error, commands.BadArgument):
+            embed = discord.Embed(description=f'{ctx.author.mention} rolled {random.randint(1, 100)}',
+                                  colour=discord.Colour.orange())
+            await ctx.send(embed=embed)
+
+    @bot.command()
+    async def top(ctx):
+        # Load user call times from the JSON file
+        with contextlib.suppress(FileNotFoundError):
+            with open('vc_time.json', 'r') as file:
+                user_call_times = json.load(file)
+        # Sort the users by time spent in a call and get the top 10
+        top_users = sorted(user_call_times.items(), key=lambda x: x[1], reverse=True)[:5]
+        title = "**Top 5 in vc:**\n"
+        description = ""
+        for i, (user_id, time_spent) in enumerate(top_users, start=1):
+            hours, remainder = divmod(time_spent, 3600)
+            minutes, seconds = divmod(remainder, 60)
+            # Use a non-pinging format for user mentions
+            description += f"**{i}.**  <@!{user_id}> - {hours}h {minutes}m {seconds}s\n"
+        age = int(time.time()) - 1679505465
+        hours2, remainder2 = divmod(age, 3600)
+        minutes2, seconds2 = divmod(remainder2, 60)
+        description += f"**Age of leaderboard:** {hours2}h {minutes2}m {seconds2}s"
+        embed = discord.Embed(title=title, description=description, colour=discord.Colour.orange())
+        await ctx.send(embed=embed)
+
+    @bot.command()
+    async def area(ctx, min, max, ratio):
+        ratio = re.sub(r",", ".", ratio)
+        description = ""
+        perfect = False
+
+        for i in range(int(min), int(max) + 1):
+            if i / float(ratio) == int(i / float(ratio)) and float(ratio) != 1.0:
+                description += f"{i} x {i / float(ratio)}\n"
+                perfect = True
+
+        if not perfect:
+            description = "no perfect values"
+            if float(ratio) == 1:
+                description = "ratio cannot be 1 to avoid spam"
+
+        embed = discord.Embed(
+            description=f"{ctx.author.mention} \nyour width values are: \nmin={min} | max={max} | ratio={ratio} \n\n{description}",
+            colour=discord.Colour.orange())
+        await ctx.send(embed=embed)
+
+    @area.error
+    async def area_error(ctx, min, max, ratio):
+        global embed
+        if isinstance(min, commands.BadArgument):
+            embed = discord.Embed(description="Please input valid min value", colour=discord.Colour.orange())
+        elif isinstance(max, commands.BadArgument):
+            embed = discord.Embed(description="Please input valid max value", colour=discord.Colour.orange())
+        elif isinstance(ratio, commands.BadArgument):
+            embed = discord.Embed(description="Please input valid ratio value", colour=discord.Colour.orange())
+            await ctx.send(embed=embed)
+
+    @bot.command()
+    async def help(ctx, command=None):
+        valid_commands = ["roll", "top", "area"]
+        if command not in valid_commands:
+            command = None
+        embed = discord.Embed(title='Help list', colour=discord.Colour.orange())
+        if command is None:
+            embed.add_field(name="!help",
+                            value="Displays a message with information about the bot's commands and functionality, including greetings and pet emotes that trigger specific responses.",
+                            inline=False)
+        if command is None or command == "roll":
+            embed.add_field(name="!roll",
+                            value="Rolls a random number between 1 and the specified number (default is 100) and displays the result in an embedded message.",
+                            inline=False)
+        if command is None or command == "top":
+            embed.add_field(name="!top",
+                            value="Displays the top 5 users who have spent the most time in a voice call, along with their total time spent and the age of the leaderboard.",
+                            inline=False)
+        if command is None or command == "area":
+            embed.add_field(name="!area",
+                            value="Calculates the perfect osu tablet area from the given with from the minimum width, maximum width and ratio values\nExample !area 80 120 1.4",
+                            inline=False)
+        if command is None:
+            embed.add_field(name="Bot greetings",
+                            value='**hello**: The bot responds with "Hello! <:scymenHey:1071540827928662037>".\n**hi** or **hai**: The bot responds with "Wassup! <:virgin:1064263792386637986>".\n**hey** or **heya**: The bot responds with "Hey there! <a:petthepainaway:1062736826617561108>".',
+                            inline=False)
+            embed.add_field(name="Emotes",
+                            value="petthepainaway: <a:petthepainaway:1062736826617561108>\npetkizu: <a:petkizu:1069555999633059840>\nnedpet: <a:nedpet:1069621576930164858>\npetelle: <a:petelle:1069558979216281682>\nkaypet: <a:kaypet:880356602803261462>\npetrainy: <a:petrainy:1062735394132742205>",
+                            inline=False)
+        await ctx.send(embed=embed)
+
+    @bot.event
+    async def on_message(message):
+        print(f'{message.author}: {message.content} | {message.channel}')
+        if message.author.id == bot.user.id:
+            return
+
+        greetings = {
+            r'\bh+e+l+o+\b': 'Hello! <:scymenHey:1071540827928662037>',
+            r'\bh+i+\b': 'Wassup! <:virgin:1064263792386637986>',
+            r'\bh+a+i+\b': 'Wassup! <:virgin:1064263792386637986>',
+            r'\bh+e+y+\b': 'Hey there! <a:petthepainaway:1062736826617561108>',
+            r'\bh+e+y+a+\b': 'Hey there! <a:petthepainaway:1062736826617561108>'
+        }
+
+        for greet, response in greetings.items():
+            if re.search(greet, message.content, re.IGNORECASE):
+                await message.channel.send(response)
+
+        simple_responses = {
+            'owo': "*owo what's this?*",
+            'uwu': 'uwu'
+        }
+
+        for msg, response in simple_responses.items():
+            if message.content.lower() == msg:
+                await message.channel.send(response)
+
+        pet_emotes = {
+            ':petthepainaway:': '<a:petthepainaway:1062736826617561108>',
+            ':Petkizu:': '<a:Petkizu:1069555999633059840>',
+            ':nedpet:': '<a:nedpet:1069621576930164858>',
+            ':Petelle:': '<a:Petelle:1069558979216281682>',
+            ':KayPet:': '<a:KayPet:880356602803261462>',
+            ':Petrainy:': '<a:Petrainy:1062735394132742205>'
+        }
+
+        for emote, response in pet_emotes.items():
+            if emote in message.content:
+                await message.channel.send(response)
+
+        await bot.process_commands(message)
+
+    @bot.event
+    async def on_raw_reaction_add(payload):
+        message_id = payload.message_id
+        if message_id == 1080112222249943130:  # message id
+            guild_id = payload.guild_id
+            guild = discord.utils.find(lambda g: g.id == guild_id, bot.guilds)
+
+            # roles and emote names from gt test server
+            if payload.emoji.name == '💜':  # reaction emote name
+                role = discord.utils.get(
+                    guild.roles, name='stream ping')  # role name
+            elif payload.emoji.name == '🧒':
+                role = discord.utils.get(guild.roles, name='Child')
+            elif payload.emoji.name == '👴':
+                role = discord.utils.get(guild.roles, name='18+')
+            else:
+                role = discord.utils.get(guild.roles, name=payload.emoji.name)
+
+            if role is not None:
+                member = await guild.fetch_member(payload.user_id)
+                if member is not None:
+                    await member.add_roles(role)
+                    print(f'{member} got role: {role}')
+                else:
+                    print('member not found')
+            else:
+                print(f'role: {role} not found')
+                print(payload.emoji.name)
+
+    @bot.event
+    async def on_raw_reaction_remove(payload):
+        message_id = payload.message_id
+        if message_id == 1080112222249943130:  # message id
+            guild_id = payload.guild_id
+            guild = discord.utils.find(lambda g: g.id == guild_id, bot.guilds)
+
+            # roles and emote names from gt test server
+            if payload.emoji.name == '💜':  # reaction emote name
+                role = discord.utils.get(
+                    guild.roles, name='stream ping')  # role name
+            elif payload.emoji.name == '🧒':
+                role = discord.utils.get(guild.roles, name='Child')
+            elif payload.emoji.name == '👴':
+                role = discord.utils.get(guild.roles, name='18+')
+            else:
+                role = discord.utils.get(guild.roles, name=payload.emoji.name)
+
+            if role is not None:
+                member = await guild.fetch_member(payload.user_id)
+                if member is not None:
+                    await member.remove_roles(role)
+                    print(f'{member} lost role: {role}')
+                else:
+                    print('member not found')
+            else:
+                print(f'role: {role} not found')
+
+    # Dictionary to store user_id as key and time spent in vc as value
+    vc_time = {}
+
+    @bot.event
+    async def on_ready():
+        print(f'We have logged in as {bot.user}')
+        await bot.change_presence(status=discord.Status.do_not_disturb , activity=discord.Game('!help'))
+        # Load vc_time from the file if it exists
+        with contextlib.suppress(FileNotFoundError):
+            with open("vc_time.json", "r") as file:
+                loaded_data = json.load(file)
+                for user_id, time_spent in loaded_data.items():
+                    user_id = str(user_id)  # Ensure user_id is a string
+                    if user_id in vc_time:
+                        vc_time[user_id] += time_spent
+                    else:
+                        vc_time[user_id] = time_spent
+        update_vc_time.start()
+        backups_date.start()
+        if not os.path.exists('osu_links.json'):
+            with open('osu_links.json', 'w') as file:
+                file.write('{}')
+
+    @tasks.loop(seconds=1)
+    async def update_vc_time():
+        for guild in bot.guilds:
+            for vc in guild.voice_channels:
+                for member in vc.members:
+                    if member.bot:
+                        continue
+                    user_id = str(member.id)  # Ensure user_id is a string
+                    if user_id not in vc_time:
+                        vc_time[user_id] = 1
+                    else:
+                        vc_time[user_id] += 1
+                    milestones = {90000: 25, 180000: 50, 360000: 100,
+                                  900000: 250, 1800000: 500, 3600000: 1000,
+                                  9000000: 2500, 18000000: 5000, 36000000: 10000}
+                    for milestone, hours in milestones.items():
+                        if vc_time[user_id] == milestone:
+                            channel = bot.get_channel(559826045453205568) # replace with the ID of the "general" channel
+                            await channel.send(
+                                f"Congratulations! <@!{user_id}> has spent {hours} hours in a voice channel!")
+        # Save vc_time to a file
+        with open("vc_time.json", "w") as file:
+            json.dump(vc_time, file)
+
+    @tasks.loop(minutes=5)
+    async def backups_date():
+        now = datetime.datetime.now()
+        backup_file = f"backups/vc_time_backups/vc_time_{now.strftime('%Y_%m_%d')}.json"
+        if not os.path.exists(backup_file) or json.load(open("vc_time.json", "r")) != json.load(open(backup_file, "r")):
+            shutil.copy("vc_time.json", backup_file)
+
+    bot.run('MTA5MTA5MDE3NjUwMTA5NjUzMA.G4nuJn.Ud1Mvr44LZqkjDpsiwBR6ZCL4z4pic4AXqHDrQ')
+
+
+if __name__ == '__main__':
+    run()
---
+++ osu_commands.py
@@ -0,0 +1,144 @@
+import contextlib
+import os
+from ossapi import UserLookupKey, Ossapi
+import oppadc
+import requests
+import zipfile
+import shutil
+
+client_id = 24667
+client_secret = "3u3hoHG5DZ54zWWj4XRuf6a1wzXuIap5uBSqXIPT"
+
+
+class osu_stats:
+    def __init__(self, user, play_type, mode):
+        self.api = Ossapi(client_id, client_secret)
+
+        # osu play data
+        self.play = self.api.user_scores(user, play_type, include_fails=True, mode=mode, limit=1)
+        self.map_id = self.play[0].beatmap.id
+
+        # map data
+        self.beatmap = self.api.beatmap(self.map_id)
+        self.mapset_id = self.beatmap.beatmapset_id
+        self.mapset_artist = self.play[0].beatmapset.artist
+        self.mapset_creator = self.play[0].beatmapset.creator
+        self.map_diff = self.beatmap.version
+        self.map_rank_status = self.beatmap.status
+        self.map_image = self.play[0].beatmapset.covers.card
+        self.map_title = self.play[0].beatmapset.title
+        self.map_date_created = self.play[0].created_at
+        self.map_obj_count = self.beatmap.count_circles + self.beatmap.count_sliders + self.beatmap.count_spinners
+
+        # player data
+        self.player_name = self.play[0]._user.username
+        self.player_avatar = self.play[0]._user.avatar_url
+
+        # play stats
+        self.stat_acc = "{:.2f}".format(round(100 * self.play[0].accuracy, 2))
+        self.stat_n300 = self.play[0].statistics.count_300
+        self.stat_n100 = self.play[0].statistics.count_100
+        self.stat_n50 = self.play[0].statistics.count_50
+        self.stat_n_miss = self.play[0].statistics.count_miss
+        self.stat_mods = self.play[0].mods
+        self.stat_score = f'{self.play[0].score:,}'
+        self.stat_stars = round(self.api.beatmap_attributes(self.map_id, mods=self.stat_mods).attributes.star_rating, 2)
+        self.stat_rank_grade = self.play[0].rank
+        self.stat_achieved_combo = self.play[0].max_combo
+        self.stat_map_progress = "{:.1f}".format(100 * (self.stat_n300 + self.stat_n100 + self.stat_n50 + self.stat_n_miss) / self.map_obj_count)
+
+        # ___________ calc fc_acc ___________ #
+
+        max_n300 = self.map_obj_count - self.stat_n100 - self.stat_n50
+        top = 300 * max_n300 + 100 * self.stat_n100 + 50 * self.stat_n50
+        divider = 300 * (max_n300 + self.stat_n100 + self.stat_n50)
+        self.stat_fc_acc = "{:.2f}".format(round(100 * (top / divider), 2))
+        # ___________ calc fc_acc ___________ #
+
+        # ___________ calc pp ___________ #
+
+        folder_path = 'map_files'
+        with contextlib.suppress(FileNotFoundError):
+            # Remove the folder and its contents
+            shutil.rmtree(folder_path)
+        # Create a new folder
+        os.mkdir(folder_path)
+
+        response = requests.get(f'https://beatconnect.io/b/{self.mapset_id}')
+
+        mapset_download = f'map_files/{self.mapset_id} {self.mapset_artist} - {self.map_title}.zip'
+
+        with open(mapset_download, 'wb') as file:
+            file.write(response.content)
+
+        with zipfile.ZipFile(mapset_download, 'r') as zip_ref:
+            osu_files = zip_ref.namelist()
+            for file in osu_files:
+                if file.endswith(f'[{self.map_diff.rstrip("?")}].osu'):
+                    zip_ref.extract(file, 'map_files')
+                    map_extract = file
+                    break
+
+
+        self.MapInfo = oppadc.OsuMap(file_path=f'map_files/{map_extract}')
+
+        self.stat_pp = round(self.MapInfo.getPP(str(self.stat_mods), recalculate=True, **{'n300': int(self.stat_n300),
+                                                                                        'n100': int(self.stat_n100),
+                                                                                        'n50': int(self.stat_n50),
+                                                                                        'combo': int(self.stat_achieved_combo)}).total_pp, 2)
+
+        self.stat_fc_pp = round(self.MapInfo.getPP(str(self.stat_mods), recalculate=True, **{'n300': int(max_n300),
+                                                                                        'n100': int(self.stat_n100),
+                                                                                        'n50': int(self.stat_n50)}).total_pp, 2)
+
+        # ___________ calc pp ___________ #
+
+        self.map_ar = round(self.api.beatmap_attributes(self.map_id, mods=self.stat_mods).attributes.approach_rate, 2)
+        self.map_od = round(self.api.beatmap_attributes(self.map_id, mods=self.stat_mods).attributes.overall_difficulty,2)
+        self.map_hp = self.beatmap.drain
+        self.map_cs = self.beatmap.cs
+        self.map_bpm = self.beatmap.bpm
+
+        if 'HR' in str(self.stat_mods):
+            self.map_hp = min(int(self.beatmap.drain) * 1.4, 10)
+            self.map_cs = min(int(self.beatmap.cs) * 1.4, 10)
+
+        if 'EZ' in str(self.stat_mods):
+            self.map_hp = int(self.beatmap.drain) * 0.5
+            self.map_cs = int(self.beatmap.cs) * 0.5
+
+        if 'DT' in str(self.stat_mods):
+            self.map_bpm = int(self.beatmap.bpm) * 1.5
+
+        if 'HT' in str(self.stat_mods):
+            self.map_bpm = int(self.beatmap.bpm) * 0.75
+
+        self.map_max_combo = self.MapInfo.maxCombo()
+
+        self.mod_int_value = 0
+
+        mod_values = {
+            'NF': 1,
+            'EZ': 2,
+            'TD': 4,
+            'HD': 8,
+            'HR': 16,
+            'SD': 32,
+            'DT': 64,
+            'RX': 128,
+            'HT': 256,
+            'NC': 576,
+            'FL': 1024,
+            'SO': 4096,
+            'PF': 16416
+        }
+
+        for mod in mod_values:
+            if mod in str(self.stat_mods):
+                self.mod_int_value += mod_values[mod]
+
+
+class linking:
+    def __init__(self, string):
+        self.api = Ossapi(client_id, client_secret)
+        self.user = self.api.user(string, key=UserLookupKey.USERNAME)
