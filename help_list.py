0b9e0bc7dcd1700b1d12377a9642bcb71783cc2d
GhostTales <danjuulchristensen1@gmail.com>
2024-11-25 03:44:31 +0100

    update help list

--- help_list.py
+++ help_list.py
@@ -20,11 +20,15 @@ commands_info = [
             "description": "Displays your most recent score in osu or of the user specified after the command.\nExample: !rs Ghost-Tales",
         },
         {
-            "name": "Greetings",
-            "description": '**hello**: The bot responds with "Hello! <:scymenHey:1071540827928662037>".\n**hi** or **hai**: The bot responds with "Wassup! <:virgin:1064263792386637986>".\n**hey** or **heya**: The bot responds with "Hey there! <a:petthepainaway:1062736826617561108>".',
+            "name": "!osu",
+            "description": "Displays your osu profile stats or of the user specified after the command.\nExample: !osu Ghost-Tales",
         },
         {
-            "name": "Emotes",
-            "description": "petthepainaway: <a:petthepainaway:1062736826617561108>\npetkizu: <a:petkizu:1069555999633059840>\nnedpet: <a:nedpet:1069621576930164858>\npetelle: <a:petelle:1069558979216281682>\nkaypet: <a:kaypet:880356602803261462>\npetrainy: <a:petrainy:1062735394132742205>",
-        },
+            "name": "Greetings",
+            "description": '**hello**: The bot responds with "Hello! <:scymenHey:1071540827928662037>".\n**hi** or **hai**: The bot responds with "Wassup! <:virgin:1064263792386637986>".\n**hey** or **heya**: The bot responds with "Hey there! <a:petthepainaway:1062736826617561108>".',
+        }#,
+        #{
+        #    "name": "Emotes",
+        #    "description": "petthepainaway: <a:petthepainaway:1062736826617561108>\npetkizu: <a:petkizu:1069555999633059840>\nnedpet: <a:nedpet:1069621576930164858>\npetelle: <a:petelle:1069558979216281682>\nkaypet: <a:kaypet:880356602803261462>\npetrainy: <a:petrainy:1062735394132742205>",
+        #},
     ]
